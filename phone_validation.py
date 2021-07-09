74b7ec3202f2c1be30ef1080dc4dcf83acb59df7
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-07-09 20:47:26 +0000

    Modified phone_validation.py to make sure the validity and spam values are always either True or False when number found in database first
    Co-authored-by: Chris Berniel Cobashatse <cc4536@columbia.edu>

--- phone_validation.py
+++ phone_validation.py
@@ -95,12 +95,14 @@ def load_database(dbName, fileName):
 
 def check_database_input(phone_number, dataframe):
     phone_format = str(phone_number)
+    bool_column_names = ['Validity', 'Spam']
+    dataframe[bool_column_names] = dataframe[bool_column_names].astype(bool)
     result = dataframe[dataframe['Phone Number'] == phone_format]
     return (len(result.index) != 0), result
 
 
 def check_validity(dataframe):
-    if dataframe['Validity'].item() == 1:
+    if dataframe['Validity'].item() == True:
         print(dataframe)
 
     else:
