14982d3741440c9c809682b92757ea7a873c1079
Axel Irikungoma <axel.irikungoma@vanderbilt.edu>
2021-07-09 01:44:45 +0000

    setting up code for heatmap
    Co-authored-by: Chris Berniel Cobashatse <cc4536@columbia.edu>

--- ds_analysis.py
+++ ds_analysis.py
@@ -1,14 +1,51 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+# Displaying full dataframe upon calling print()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 spam_numbers = pd.read_csv("dnc_complaint_numbers_2021-07-08.csv")
 
-calls_by_state = spam_numbers.groupby('Consumer_State').size()
-print(type(calls_by_state))
-print(calls_by_state.index[0])
-print(calls_by_state[0])
+calls_num = spam_numbers.groupby(['Subject', 'Consumer_State']).size()
+# print(calls_num)
 
-import plotly.graph_objects as go  # import plotly
-fig = go.Figure(data=go.Bar(x=calls_by_state.index, y=calls_by_state)) # create a figure
-fig.write_html('figure.html') # export to HTML file
+x_list = []
+y_list = []
+data_list = []
+
+# putting states in x_list
+for state in spam_numbers['Consumer_State']:
+  x_list.append(state)
+new_x_list1 = list(set(x_list))
+new_x_list2 = [x for x in new_x_list1 if pd.isnull(x) == False]
+new_x_list2.sort()
+
+# putting subjects in y_list
+for subject in spam_numbers['Subject']:
+  y_list.append(subject)
+y_list = list(dict.fromkeys(y_list))
+new_y_list = [y for y in y_list if (y != 'Other' and y != 'No Subject Provided')]
+new_y_list.sort()
+
+# putting numbers in data
+for subject in new_y_list:
+  subj = []
+  for state in new_x_list2:
+    if state in calls_num[subject]:
+      subj.append(calls_num[subject][state])
+    else:
+      subj.append(0)
+  data_list.append(subj)
+
+# plotting
+import plotly.graph_objects as go
+map_ = go.Figure(data=go.Heatmap(z = data_list, x = new_x_list2, y=new_y_list)) # create a figure
+map_.update_layout(title='Spam Calls per State and Subject')
+map_.write_html('map.html') # export to HTML file
+
+
